b368a6bca4ada6be393ef0d2f9cd1eb808e96fd6
ComondanteChe <155365815+ComondanteChe@users.noreply.github.com>
2025-10-05 21:54:01 +0700

    Update ansible.tf

--- ansible.tf
+++ ansible.tf
@@ -1,13 +1,8 @@
-data "template_file" "ansible_inventory" {
-  template = file("${path.module}/hosts.tftpl")
-    vars = {
-    webservers = [yandex_compute_instance.web[*]]
-    database  = [yandex_compute_instance.each_vm_instance[*]]
-    storage   = yandex_compute_instance.storage.name
-    }
-}
-
-resource "local_file" "hosts_templatefile" {
-  content = data.template_file.ansible_inventory.vars[*]
+resource "local_file" "ansible_inventory" {
+  content = templatefile("${path.module}/hosts.tftpl",
+    { webservers = [yandex_compute_instance.web[*]]
+      database  = [yandex_compute_instance.each_vm_instance[*]]
+      storage   = yandex_compute_instance.storage.name
+    })
   filename = "${abspath(path.module)}/hosts.ini"
 }
