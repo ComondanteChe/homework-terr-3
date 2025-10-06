77c92b006c6a5aa3e2e0990595ed77fdf01f4dc3
ComondanteChe <155365815+ComondanteChe@users.noreply.github.com>
2025-10-06 11:09:09 +0700

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,16 @@
 output "VM_info" {
-value = [
-    for vm in yandex_compute_instance.each_vm_instance : {
+value = marge (
+    {for key, vm in yandex_compute_instance.each_vm_instance : "${key}" => {
       id          = vm.id
       name        = vm.name
       fqdn        = vm.fqdn
     }
-]
+    },
+    {for idx, vm in yandex_compute_instance.web : "${idx}" => {
+      id          = cm.id
+      name        = cm.name
+      fqdn        = cm.fqdn
+    }
+    }
+)
 }
