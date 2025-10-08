32f46527ec0aedf8c0c7af9b5321ecaf002a9e25
ComondanteChe <155365815+ComondanteChe@users.noreply.github.com>
2025-10-08 11:42:38 +0700

    Update ansible.tf

--- ansible.tf
+++ ansible.tf
@@ -22,6 +22,6 @@ resource "null_resource" "ansible_provision" {
     playbook_src_hash = file("${abspath(path.module)}/test.yml") # при изменении содержимого playbook файла
     # ssh_public_key = var.public_key # при изменении переменной with ssh 
     template_rendered = "${local_file.ansible_inventory.content}" #при изменении inventory-template
-    password_change = jsonencode( {for k,v in random_password.each: k=>v})
+    # password_change = jsonencode( {for k,v in random_password.each: k=>v})
   }
 }
